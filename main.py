7c46ec3ae7c4c2b3bfcedbdb0a0e813407e606ac
Ethanfel <ethan.fel@ts-pc.fr>
2026-04-06 11:11:25 +0200

    feat: project skeleton

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import Qt
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("8-cut")
+        self.resize(900, 650)
+
+
+if __name__ == "__main__":
+    main()
